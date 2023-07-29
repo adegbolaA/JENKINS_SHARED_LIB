fd3d8dbf0cc76c5b59f096b6fc63f4979c2910c7
EC2 Default User <ec2-user@ip-172-31-4-56.us-east-2.compute.internal>
2023-07-29 05:54:06 +0000

    adding files for ECR

--- vars/dockerImageCleanUp.groovy
+++ vars/dockerImageCleanUp.groovy
@@ -2,7 +2,6 @@ def call(String project, String ImageTag, String dockerHubUser){
     sh """
        
         docker rmi ${dockerHubUser}/${project}:${ImageTag} 
-        docker pull ${dockerHubUser}/${project}:latest
-        docker rmi ${dockerHubUser}/${project} ${dockerHubUser}/${project}:latest
+        docker image prune -a
     """
 }
