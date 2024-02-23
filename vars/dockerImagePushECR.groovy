014bc91a2615d5f9b5211c44711cf79eb6a0c76a
adegbola <117126180+adegbolaA@users.noreply.github.com>
2024-02-23 08:25:32 -0600

    Update dockerImagePushECR.groovy

--- vars/dockerImagePushECR.groovy
+++ vars/dockerImagePushECR.groovy
@@ -1,19 +1,8 @@
 def call(String aws_account_id, String region, String ecr_repoName){
-    // sh """
-    //     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-    //     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+   sh """
+        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+         docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
         
        
-    // """
-
-    // Check if AWS CLI and Docker are installed
-    sh "which aws && which docker"
-    sh "aws --version"
-    
-    // Login to ECR and push the Docker image
-    sh """
-        set +x
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com || { echo 'ECR login failed'; exit 1; }
-        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest || { echo 'Docker push failed'; exit 1; }
-    """
+     """
 }
